ae7fe6bf628a5525110491d47689fcb41cd61135
10994015 <a0938599191@gmail.com>
2023-06-28 23:38:15 +0800

    06287

--- resources/views/components/home.blade.php
+++ resources/views/components/home.blade.php
@@ -87,7 +87,7 @@
         <div class="content">
             <div class="personal">
                 <div class="image">
-                    <canvas id="me" style="display:block"></canvas>
+                    <canvas id="me" style="display:none"></canvas>
                     <img src="/images/me.gif" />
                 </div>
                 <div class="balloon-tip">
